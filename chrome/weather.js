5ecd1ef3180288f4d28ec4599922556dc77e779d
mdgarden <chooi9522@gmail.com>
2020-10-19 00:19:10 +0900

    #3.9 Getting the Weather part Two API

--- chrome/weather.js
+++ chrome/weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "99cd5b30d9936ceec8426f337594342b";
-const COORDS = 'coords';
+const COORDS = "coords";
+
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,10 +27,11 @@ function handleGeoSuccess(position){
         longitude:longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
-    console.log("Error");
+    console.log("Geo location Error");
 }
 
 function askForCoords(){
@@ -28,7 +43,8 @@ function loadCoords(){
     if(loadedCoords == null){
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitiude, parseCoords.longitude);
     }
 }
 
